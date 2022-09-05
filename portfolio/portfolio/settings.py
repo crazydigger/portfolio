8c7065b1482e4682e8634143d20424fd54e4ea10
borovirp <40pobGsb>
2022-09-05 15:23:11 +0300

    update added

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-lj#oe=dv8(v-$$sexkm1lx5h2eywn*&!954gzkbz7^byx3xu0e'
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 #DEBUG = True
